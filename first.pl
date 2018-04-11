2ebcbec3d822001e2c16137e5dec447a0ed2c419
aishwaram <36253798+aishwaram@users.noreply.github.com>
2018-04-11 22:38:49 +0530

    PERL

---
+++ first.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+
+print "Hello, world\n";
+
+$str="My name is Aishwarya";
+print "$str\n";
+$str='Aishwarya Ramesh K, \n';
+print "$str\n";
+
+$str="\uthis \ut will be uppercase";
+print "$str\n";
+
+$str="\U Everything is capital";
+print "$str\n";
+
+$str="\U selected \E is capital";
+print "$str\n";
+
+$age = 25;            
+$name = "James";    
+$salary = 1300.50;    
+
+print "Age = $age\n";
+print "Name = $name\n";
+print "Salary = $salary\n";
+
+@ages=(25,30,40);
+@names=("James","Logan","Felix","Ryan");
+
+
+
+
+%data = ('John Paul', 45, 'Lisa', 30, 'Kumar', 40);
+
+print "\$data{'John Paul'} = $data{'John Paul'}\n";
+print "\$data{'Lisa'} = $data{'Lisa'}\n";
+print "\$data{'Kumar'} = $data{'Kumar'}\n";
+
